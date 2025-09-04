54bddebabb61ce9d3b2fceeab1c649a21d1748a1
Pablo Pérez <pablo.eluniversoweb@gmail.com>
2025-09-03 22:05:26 -0300

    hecho en clase

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -17,10 +17,10 @@ class ProductoController extends Controller
                 'id' => $producto->id,
                 'nombre' => $producto->nombre,
                 'descripcion' => $producto->descripcion,
-                'stock' => $producto->stock,
+                //'stock' => $producto->stock,
                 'precio' => $producto->precio,
-                'peso' => $producto->peso,
-                'disponible' => $producto->disponible,
+                //'peso' => $producto->peso,
+                //'disponible' => $producto->disponible,
                 'categoria' => $producto->categoria->nombre, // solo el nombre
             ];
         });
@@ -32,7 +32,12 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Crea el producto directamente con los datos recibidos
+        $producto = Producto::create($request->all());
+        return response()->json([
+            'message' => 'Producto creado correctamente',
+            'producto' => $producto
+        ], 201);
     }
 
     /**
@@ -50,7 +55,12 @@ class ProductoController extends Controller
      */
     public function update(Request $request, Producto $producto)
     {
-        //
+        // Actualiza el producto con todos los datos recibidos
+        $producto->update($request->all());
+        return response()->json([
+            'message' => 'Producto actualizado correctamente',
+            'producto' => $producto
+        ], 200);
     }
 
     /**
@@ -58,6 +68,9 @@ class ProductoController extends Controller
      */
     public function destroy(Producto $producto)
     {
-        //
+        $producto->delete();
+        return response()->json([
+            'message' => 'Producto eliminado correctamente'
+        ], 200);
     }
 }
