b121ba9861536aa64d30c45e6b7a00e1df257fec
Pablo Pérez <pablo.eluniversoweb@gmail.com>
2025-09-19 18:56:37 -0300

    JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
@@ -44,6 +46,73 @@ class AuthController extends Controller
             return response()->json([
                 'success' => "error",
                 'message' => 'Ocurrió un error al crear el usuario',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    //Login
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required|string|min:6',
+            ]);
+
+            $credentials = $request->only('email', 'password');
+
+            // Intentar generar token
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Credenciales inválidas'
+                ], 401);
+            }
+
+            // Expiración del token
+            //$expiresIn = JWTAuth::factory()->getTTL() * 60;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login exitoso',
+                'token' => $token,
+                'token_type' => 'bearer',
+               // 'expires_in' => $expiresIn
+            ]);
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No se pudo crear el token',
+                'error' => $e->getMessage()
+            ], 500);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error de validación',
+                'errors' => $e->errors()
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error de servidor',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function logout()
+    {
+        try {
+            JWTAuth::parseToken()->invalidate();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout exitoso'
+            ]);
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No se pudo cerrar sesión',
                 'error' => $e->getMessage()
             ], 500);
         }
--- routes/api.php
+++ routes/api.php
@@ -7,20 +7,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categorias', CategoriaController::class);
-//Route::apiResource('productos', ProductoController::class);
+//Route::apiResource('productos', ProductoController::class)->middleware('auth:api');
 
 // GET → Listar todos
-Route::get('productos', [ProductoController::class, 'index']);
+//Route::get('productos', [ProductoController::class, 'index'])->middleware('auth:api');
 // POST → Crear nuevo
-Route::post('productos', [ProductoController::class, 'store']);
+//Route::post('productos', [ProductoController::class, 'store']);
 // GET → Mostrar uno en particular
-Route::get('productos/{producto}', [ProductoController::class, 'show']);
+//Route::get('productos/{producto}', [ProductoController::class, 'show']);
 // PUT → Actualizar completamente
-Route::put('productos/{producto}', [ProductoController::class, 'update']);
+//Route::put('productos/{producto}', [ProductoController::class, 'update']);
 // PATCH → Actualizar parcialmente
-Route::patch('productos/{producto}', [ProductoController::class, 'update']);
+//Route::patch('productos/{producto}', [ProductoController::class, 'update']);
 
 // DELETE → Eliminar
-Route::delete('productos/{producto}', [ProductoController::class, 'destroy']);
+//Route::delete('productos/{producto}', [ProductoController::class, 'destroy']);
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:api']], function () {
+ Route::get('/productos', [ProductoController::class, 'index']);
+ Route::post('/logout', [AuthController::class, 'logout']);
+});
